302523900e0284f581ea58df370b202c18edb144
dermotmoloneyacc <44987428+dermotmoloneyacc@users.noreply.github.com>
2019-07-29 18:19:25 -0500

    adding baseball class for lab8

---
+++ baseball.py
@@ -0,0 +1,18 @@
+class baseball():
+    def single(self,x):
+        y=0
+        y=x*1
+        return (y)
+
+    def double(self,x):
+        y=0
+        y=x*2
+        return(y)
+    def triple(self,x):
+        y=0
+        y=x*3
+        return(y)
+    def homerun(self,x):
+        y=0
+        y=x*4
+        return(y)
